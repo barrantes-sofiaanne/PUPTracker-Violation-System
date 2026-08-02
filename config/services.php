58bc7b01349cd002f5ce353d7c1f59eb3bcec3c6
barrantes-sofiaanne <sabarrantes2911@gmail.com>
2026-08-02 21:38:17 +0800

    Change input for mailer

--- config/services.php
+++ config/services.php
@@ -13,7 +13,11 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
-
+    'mailgun' => [
+    'domain' => env('MAILGUN_DOMAIN'),
+    'secret' => env('MAILGUN_SECRET'),
+    'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+],
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
